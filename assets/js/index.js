9a03b7b54128cdf6c96ad44504b61df065b50e77
EvgenStr <juve2910@gmail.com>
2021-04-01 13:37:23 +0300

    add getmin and getmax function

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,24 @@
+'use strict';
+// Даны числа 4, -2, 5, 19, -130, 0, 10. Найдите минимальное и максимальное число.
 
+const arr = [4, -2, 5, 19, -130, 0, 10];
 
+function getMax(array) {
+  let result = array[0];
+  for (let i = 1; i < array.length; i++) {
+    if (array[i] > result) result = array[i];
+  }
+  return result;
+}
+
+function getMin(array) {
+  let result = array[0];
+  for (let i = 1; i < array.length; i++) {
+    if (array[i] < result) result = array[i];
+  }
+  return result;
+}
+console.log(`min - ${getMin(arr)};\nmax ${getMax(arr)}`)
 
 
 
