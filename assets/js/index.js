18a6cc3bd8c76afb70b5bb857f23660bb833055f
EvgenStr <juve2910@gmail.com>
2021-04-01 14:11:51 +0300

    add function average

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,6 +3,7 @@
 
 //*** ver 1 ***   
 const arr = [4, -2, 5, 19, -130, 0, 10];
+const arr2 = [12, 15, 20, 25, 59, 79];
 
 /**
  * get maximum value from the array
@@ -29,12 +30,12 @@ function getMin(array) {
   }
   return result;
 }
-console.log(`min : ${getMin(arr)};\nmax : ${getMax(arr)}`)
+console.log(`min : ${getMin(arr)};\nmax : ${getMax(arr)}`);
 
 //*** ver 2 ***     
 
-console.log(Math.min.apply(null, arr));
-console.log(Math.max.apply(null, arr));
+console.log('Math.min.apply:', Math.min.apply(null, arr));
+console.log('Math.max.apply:', Math.max.apply(null, arr));
 
 //*** ver 3 ***     
 
@@ -46,10 +47,24 @@ function getMinV3() {
   }
   return result;
 }
-const arr2 = [14, -22, 51, 1, -13, 0, -10];
 
 console.log("Version 3 min:", arr.min());
-console.log( 'arr.min() === arr2.min() = ', arr.min() === arr2.min());
+console.log('arr.min() === arr2.min() = ', arr.min() === arr2.min());
+
+/*
+Дан массив arr. Найдите среднее арифметическое его элементов. Проверьте задачу на массиве с элементами 12, 15, 20, 25, 59, 79.
+*/
+const arr3 = [12, 15, 20, 25, 59, 79];
+
+function getAverage(array) {
+  let sum = null;
+  for (let i = 0; i < array.length; i++) {
+    sum += array[i];
+  }
+  return (sum / array.length);
+}
+console.log('Average: ', getAverage(arr3));
+
 
 
 // if (!new.target) {
