8cff6f6f8422eefe3103b30b22a96dfbe2dddbeb
EvgenStr <juve2910@gmail.com>
2021-04-01 13:45:54 +0300

    add variant 2

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,8 +1,14 @@
 'use strict';
 // Даны числа 4, -2, 5, 19, -130, 0, 10. Найдите минимальное и максимальное число.
 
+//*** ver 1 ***   
 const arr = [4, -2, 5, 19, -130, 0, 10];
 
+/**
+ * get maximum value from the array
+ * @param {array} array 
+ * @returns {number}
+ */
 function getMax(array) {
   let result = array[0];
   for (let i = 1; i < array.length; i++) {
@@ -11,6 +17,11 @@ function getMax(array) {
   return result;
 }
 
+/**
+ * get minimum value from the array
+ * @param {array} array 
+ * @returns {number}
+ */
 function getMin(array) {
   let result = array[0];
   for (let i = 1; i < array.length; i++) {
@@ -18,9 +29,12 @@ function getMin(array) {
   }
   return result;
 }
-console.log(`min - ${getMin(arr)};\nmax ${getMax(arr)}`)
+console.log(`min : ${getMin(arr)};\nmax : ${getMax(arr)}`)
 
+//*** ver 2 ***     
 
+console.log(Math.min.apply(null, arr));
+console.log(Math.max.apply(null, arr));
 
 // if (!new.target) {
 //   new Accumulator(startingValue);
